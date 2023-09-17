557ca07fcd15ea4e03cc77f6a246efd248e8a4a8
Olivergisttv <victorolaonifogo@gmail.com>
2023-09-17 14:49:13 -0700

    Update

--- main.c
+++ main.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-
+#include <sys/wait.h>
+#include "main.h"
 /**
  * main - Command line Interpreter
  * Return: 0
@@ -10,19 +11,39 @@
 int main(int argc, char *argv[], char **env)
 {
 	ssize_t status;
+	int i;
 	char *lineptr = NULL;
 	size_t n = sizeof(size_t) * 1024;
 	char *args[] = {"/bin/ls", NULL};
+	pid_t child_process;
 
 	while (argc)
 	{
 		write(1, "$ ", 2);
 		status = getline(&lineptr, &n, stdin);
 		if (status == -1)
-			exit(0);
-		lineptr[strlen(lineptr) - 1] = '\0';
-		if (execve(lineptr, args, env) == -1)
-			perror(argv[0]);
+		{
+			free(lineptr);
+			exit(EXIT_SUCCESS);
+		}
+		lineptr[custom_strlen(lineptr) - 1] = '\0';
+
+		child_process = fork();
+		if (child_process == 0)
+		{
+			if (execve(lineptr, args, env) == -1)
+			{
+				perror(argv[0]);
+				free(lineptr);
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			do {
+				waitpid(child_process, &i, WUNTRACED);
+			} while (!WIFEXITED(i) && !WIFSIGNALED(i));
+		}
 	}
 	free(lineptr);
 	return (0);
---
+++ main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+
+size_t custom_strlen(const char *str);
+
+#endif /* MAIN_H */
